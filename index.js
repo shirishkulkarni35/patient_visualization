8f1310132b20a09f9c5b92badd725c374a239f16
Shirish Kulkarni <shirishkpl109@gmail.com>
2022-10-19 17:30:49 +0530

    comments added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/public'));
 
 
 mongoose.connect("mongodb+srv://shirish_35:shirish35@cluster0.bs1p3.mongodb.net/patient",{useNewUrlParser:true},{useUnifiedTopology:true});
-// create a adata schema
+// create a data schema
 const patientSchema = {
     fName: String,
     lName: String,
@@ -27,12 +27,13 @@ const patientSchema = {
 }
 const patient_model = mongoose.model("patient_details", patientSchema);
 
-
+// Route to display the Homepage
 app.get('/', function(req, res) {
     console.log('Hello World');
     res.sendFile(__dirname + 'D:\practiceProjects\public/index.html');
 });
 
+//Posting form data to MongoDB
 app.post("/",function(req,res){
     let newPatient = new patient_model({
         fName: req.body.fName,
@@ -52,19 +53,22 @@ app.post("/",function(req,res){
 
     });
     newPatient.save();
-    res.redirect("/");
+    res.redirect("/"); // To stay in the homepage after submitting the form
 });
 
+// Retriving all the Patient data from the Database
 app.get("/patientData", async(req, res) => {
     const getAllPatient = await patient_model.find();
     return res.json(getAllPatient);
 });
 
+// Displaying the Pie-Chart 
 app.get("/patientVisuals",function(req,res){
     console.log("hello world");
     res.sendFile(__dirname + '/public/chart.html');
 })
 
+// Listening requests on port 3000
 app.listen(3000, function(){
     console.log("Server is running");
 })
